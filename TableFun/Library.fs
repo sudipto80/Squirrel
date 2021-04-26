68e8ee7a66913b1ba9c92025cfdd99db22607ffb
Sudipta Mukherjee <Sudipta@raincode.com>
2021-04-26 23:41:10 +0530

    F# friendly wrapper (the start)

---
+++ TableFun/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace Squirrel.FSharp
+open Squirrel
+open System
+
+module Table =
+    //Wrapper for Pick() from Squirrel.Table
+    let pick ([<ParamArray>] columns : string array) (x:Table) =
+        x.Pick(columns)
+
+    let drop ([<ParamArray>] columns : string array) (x:Table) =
+        x.Pick(columns)
+
+    let addColumn(columnName:string)(formula:string)(decimalDigits:int)(x:Table)=
+        x.AddColumn(columnName,formula,decimalDigits)
+
+    let aggregate(columnName:string)(how:AggregationMethod)(x:Table)=
+        x.Aggregate(columnName,how)
+
+    let roundOffTo(decimalDigits:int)(x:Table) = x.RoundOffTo(decimalDigits)
+
+    let prettyDump (x:Table) = x.PrettyDump()
