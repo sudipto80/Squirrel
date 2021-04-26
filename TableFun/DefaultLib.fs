42fb5d0a15ff458fa4c255c379c4f1c27e8ade4a
Sudipta Mukherjee <Sudipta@raincode.com>
2021-04-27 00:15:37 +0530

    Placeholder for default argument version of Table (to be wrapped for F#)

---
+++ TableFun/DefaultLib.fs
@@ -0,0 +1,14 @@
+﻿
+//Home for all Table functionalities that 
+//have a default value for one or more of the arguments
+namespace Squirrel.FSharp.Defaults
+open Squirrel
+
+
+module Table =
+
+//When no argument is provided to Aggregate it uses "Sum" 
+//For that we needed this function 
+let aggregate(columnName:string)(x:Table)=
+      x.Aggregate(columnName,AggregationMethod.Sum)
+
