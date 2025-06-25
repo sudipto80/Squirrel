c0bd2f3054386df8ff2f7a8e4325aab7a5ce88de
Sudipta Mukherjee <sudipto80@yahoo.com>
2025-06-25 23:55:50 +0530

    SquirrelSolution
    To work with all other projects

--- SquirrelSolution/ConsoleDemo/Program.cs
+++ SquirrelSolution/ConsoleDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Drawing;
+using IronOcr;
 using Squirrel;
 using Squirrel.Cleansing;
 using TableAPI;
@@ -30,28 +31,40 @@ using TableAPI;
 // cleanEmps.PrettyDump();
 //
 
-var badCombs = DataAcquisition.LoadCsv("C:\\Users\\Admin\\Documents\\GitHub\\Squirrel\\SquirrelSolution\\ConsoleDemo\\badComb.csv");
+// var badCombs = DataAcquisition.LoadCsv("C:\\Users\\Admin\\Documents\\GitHub\\Squirrel\\SquirrelSolution\\ConsoleDemo\\badComb.csv");
+//
+// Dictionary<string, HashSet<string>> combinations
+//     = new Dictionary<string, HashSet<string>>();
+//
+//
+//
+// //Electronics and Textile can't have Liters as the unit 
+// Func<string, string, bool> badCom1 = (x,y) => x.Equals("Electronics") && y.Equals("Liters");
+// Func<string, string, bool> badCom2 = (x,y) => x.Equals("Textiles") && y.Equals("Liters");
+//
+// Func<string,string,bool> categoryUnitViolations = 
+//     (x,y) => (x.Equals("Electronics") || x.Equals("Textiles")) && y.Equals("Liters");
+//
+// Func<string,string,bool> countryCurrencyViolations = 
+//     (x,y) => (x.Equals("USA") || x.Equals("US")) && !y.Equals("USD");
+//
+// List<Func<string,string,bool>> badCombinations = [badCom1, badCom2];
+//
+//
+//     
+//
+//
+// badCombs
+//         //.RemoveCombination("Category", "Unit", badCom1)
+//         //.RemoveCombination("Category","Unit", badCom2)
+//         .RemoveCombination("Category","Unit", categoryUnitViolations)
+//         .RemoveCombination("Country","Currency", countryCurrencyViolations)
+//         .PrettyDump(rowColor:ConsoleColor.Red);
+//
+// Console.WriteLine(badCombs.RowCount);
 
-Dictionary<string, HashSet<string>> combinations
-    = new Dictionary<string, HashSet<string>>();
-
-
-
-//Electronics and Textile can't have Liters as the unit 
-Func<string, string, bool> badCom1 = (x,y) => x.Equals("Electronics") && y.Equals("Liters");
-Func<string, string, bool> badCom2 = (x,y) => x.Equals("Textiles") && y.Equals("Liters");
-
-Func<string,string,bool> categoryUnitViolations = 
-    (x,y) => (x.Equals("Electronics") || x.Equals("Textiles")) && y.Equals("Liters");
-List<Func<string,string,bool>> badCombinations = [badCom1, badCom2];
-
-    
-
-
-badCombs
-        //.RemoveCombination("Category", "Unit", badCom1)
-        //.RemoveCombination("Category","Unit", badCom2)
-        .RemoveCombination("Category","Unit", categoryUnitViolations)
-        .PrettyDump(rowColor:ConsoleColor.Red);
-
-Console.WriteLine(badCombs.RowCount);
+Aspose.OCR.AsposeOcr recognitionEngine = new Aspose.OCR.AsposeOcr();
+Aspose.OCR.OcrInput source = new Aspose.OCR.OcrInput(Aspose.OCR.InputType.SingleImage);
+source.Add("C:\\Users\\Admin\\Downloads\\receipt2.jpg");
+Aspose.OCR.OcrOutput results = recognitionEngine.Recognize(source);
+Console.WriteLine(results[0].RecognitionText);
